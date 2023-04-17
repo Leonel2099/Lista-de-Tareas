72b6d35e5c857f8f50140499ee2b4f65c752cee2
Leonel2099 <ezequielleonel1@gmail.com>
2023-04-17 15:57:24 -0300

    Crecion de la funcion agregar a la lista

--- js/app.js
+++ js/app.js
@@ -1,25 +1,24 @@
 const formulario = document.querySelector("form");
 formulario.addEventListener("submit", cargarLista);
-
+let lista = document.querySelector("ul");
 function cargarLista(e) {
   e.preventDefault();
   let contador = 0;
   let tarea = document.getElementById("tarea").value;
   formulario.reset();
-  let lista = document.querySelector("ul");
+
   let tareaAgregar = document.createElement("li");
-  tareaAgregar.className = 'list-group-item'
+  tareaAgregar.className = "list-group-item";
   tareaAgregar.innerHTML = `<div class="d-flex justify-content-between">
   <p class="m-0 py-1">
     ${tarea}
   </p>
-  <button class="btn btn-outline-danger">
+  <button class="btn btn-outline-danger" onclick="eliminar()">
       <i class="bi bi-trash"></i>
   </button>
 </div>`;
-  lista.append(tareaAgregar);
-  let eliminar = document.getElementById("btnEliminar");
-  eliminar.addEventListener("click", () => {
-    lista.removeChild(tareaAgregar);
-  });
+  if (tarea != "") {
+    lista.append(tareaAgregar);
+  }
 }
+function eliminar() {}
